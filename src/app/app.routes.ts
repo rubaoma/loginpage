dc8cdd1e737d98de5d372c9a59d48305850686c7
RubensMa <rubao.ma@gmail.com>
2024-06-29 17:50:21 -0300

    feat: signup and new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,24 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './page/login/login.component';
+import { SignupComponent } from './page/signup/signup.component';
+import { Component } from '@angular/core';
+import { UserComponent } from './page/user/user.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 export const routes: Routes = [
     {
         path: "login",
         component: LoginComponent
+    },
+    {
+        path: "signup",
+        component: SignupComponent
+    },
+    {
+        path: "user",
+        component: UserComponent,
+        canActivate: [AuthGuardService]
     }
+
 
 ];
